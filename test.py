8d7206bab7283236908fc24c08536ebb3feb5104
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-01-29 16:57:33 +0900

    Add : test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+def Even(num):
+    new_list = []  # 빈 리스트 만들기
+    for i in range(0, 51):  # 1-50까지의 숫자를 하나씩 돌아줘
+        if num % 2 == 0:  # 1-50까지 숫자 중에서 짝수인 애는
+            new_list.append(i)  # 뉴리스트 안에 넣어줘 -> 1-50에서 짝수가 뉴 리스트 안에 담긴다
+
+    return new_list  # 뉴 리스트안에 담겨진 숫자들 - 짝수들을 리턴해줘
